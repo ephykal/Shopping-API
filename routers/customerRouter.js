ff2c9a0a6627223c76e6200e71aac1f821be323f
ephykal <ephraimolu6@gmail.com>
2023-06-06 16:29:38 -0500

    updated routes files

--- routers/customerRouter.js
+++ routers/customerRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { allCustomer, customerCreation } = require('../handlers/customerHandler');
-// const dot = require('dotenv/config');
 
 
 router.get('/', allCustomer);
