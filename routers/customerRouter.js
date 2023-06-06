53302c717b181dbdec1ae375339bfdb4f29c8375
ephykal <ephraimolu6@gmail.com>
2023-06-06 16:46:59 -0500

    updated app.js

--- routers/customerRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { allCustomer, customerCreation } = require('../handlers/customerHandler');
-
-
-router.get('/', allCustomer);
-router.post('/',customerCreation )
-
-module.exports = router;
