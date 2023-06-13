7354a18989fda72104708b364a1246d47fd73f50
ephykal <ephraimolu6@gmail.com>
2023-06-13 16:08:52 -0500

    deleted irrelevant comments

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -2,11 +2,7 @@ const express = require('express');
 const { allOrders, orderCreation } = require('../handlers/orderHandler');
 const router = express.Router();
 
-
-// getting all orders
 router.get('/', allOrders);
-
-// orders creation 
 router.post('/', orderCreation);
 
 
