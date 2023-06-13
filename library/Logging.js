d2baffaf93031ab11a0f824f3defea55691dd461
ephykal <ephraimolu6@gmail.com>
2023-06-13 11:17:38 -0500

    created a new logger that logs more information and concise error messages

--- library/Logging.js
+++ library/Logging.js
@@ -1,15 +1,48 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
 
-const logger = {
-  info: (message) => {
-    console.log(chalk.blue(message));
-  },
-  warn: (message) => {
-    console.log(chalk.yellow(message));
-  },
-  error: (message) => {
-    console.log(chalk.red(message));
-  },
-};
+// const logger = {
+//   info: (message) => {
+//     console.log(chalk.blue(message));
+//   },
+//   warn: (message) => {
+//     console.log(chalk.yellow(message));
+//   },
+//   error: (message) => {
+//     console.log(chalk.red(message));
+//   },
+// };
 
-module.exports = logger;
+// module.exports = logger;
+
+
+
+const chalk = require("chalk");
+
+class Logging {
+  static log(args) {
+    Logging.info(args);
+  }
+
+  static info(args) {
+    console.log(
+      chalk.blue(`[${new Date().toLocaleString()}],[INFO]`),
+      typeof args === "string" ? chalk.blueBright(args) : args
+    );
+  }
+
+  static warining(args) {
+    console.log(
+      chalk.yellow(`[${new Date().toLocaleString()}],[WARNING]`),
+      typeof args === "string" ? chalk.yellowBright(args) : args
+    );
+  }
+
+  static error(args) {
+    console.log(
+      chalk.red(`[${new Date().toLocaleString()}],[ERROR]`),
+      typeof args === "string" ? chalk.redBright(args) : args
+    );
+  }
+}
+
+module.exports = Logging;
