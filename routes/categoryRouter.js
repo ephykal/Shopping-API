fb85b02e53825c1f5cb7a196ac1f541929f6241a
ephykal <ephraimolu6@gmail.com>
2023-06-16 18:52:32 -0500

    modified

--- routes/categoryRouter.js
+++ routes/categoryRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Logging = require('../library/Logger');
 const { allCategories, categoryCreation } = require('../handlers/categoryHandler');
 
 
